40e0ea55a12c56175f5b1754309efb5c6082e203
Zeinali Ali <ali.zeinali@zollner.de>
2025-05-23 09:38:03 +0200

    delete and create new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterOutlet, LoginComponent],
+  template: `
+    <!-- <app-homepage /> -->
+     <app-login/>
+    <router-outlet />
+  `,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Angular-Net';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+//import { HomepageComponent } from './home/homepage/homepage.component';
 
 export const routes: Routes = [
-  //{ path: '', component: HomepageComponent }, // default route
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () => {
-      return import('./Home/homepage/homepage.component').then((m) => m.HomepageComponent)
-    },
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () => {
-      return import('./Home/user/user.component').then((u) => u.UserComponent)
-    },
-  }
+  { path: '', component: LoginComponent }, // default route
+  
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   loadComponent: () => {
+  //     return import('./home/user/user.component').then((u) => u.UserComponent)
+  //   },
+  // }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {
 
-}
+// }
